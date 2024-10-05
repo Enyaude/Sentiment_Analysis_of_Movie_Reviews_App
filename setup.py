5834a1f50c46ade929a24265100d96bb9e0a5d33
unknown <enyaudesamuel@hotmail.com>
2024-10-05 23:16:43 +0100

    first commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Sentiment_Analysis_of_Movie_Reviews_App',
+    version='0.4',
+    license='MIT',
+    description='Python, Scikit-learn, Jupyter Notebook package',
+    install_requires=['SQL', 'Mysql', 'JupyterNotebook'],
+    url='https://github.com/Enyaude/Sentiment_Analysis_of_Movie_Reviews_App.git',
+    author='<Enyaude>',
+    author_email='<enyaudesamuel@hotmail.com>'
+    ) 
